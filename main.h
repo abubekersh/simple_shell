a244364fb2acfc11465ab6e796129626f9a79950
abubeker <abakabrak@gmail.com>
2024-02-28 09:08:19 -0800

    basic shell only using execve

--- main.h
+++ main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+#define  MAX_LENGTH 100
 
 #endif
--- shell.c
+++ shell.c
@@ -1,15 +1,47 @@
-#include <stdio.h>
-#include <unistd.h>
-
+#include "main.h"
 /**
  * main - entry point of the program
  * Return: int
  */
+
 int main(void)
 {
-	pid_t pid;
+	extern char **environ;
+	pid_t child;
+	int status;
+	char input[MAX_LENGTH];
+	char *args[2];
 
-	pid = getpid();
-	printf("the id of this proccess is %u\n", pid);
+	while (1)
+	{
+		printf("#cisfun$ ");
+		if (fgets(input, MAX_LENGTH, stdin) == NULL)
+		{
+			printf("\n");
+			return (-1);
+		}
+		input[strcspn(input, "\n")] = '\0';
+		args[0] = input;
+		args[1] = NULL;
+		child = fork();
+		if (child == -1)
+		{
+			perror("./shell");
+			return (-1);
+		}
+		else if (child == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				fprintf(stderr, "execve failed: %s\n", strerror(errno));
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+
 	return (0);
 }
